d97f4242c27030f2500b2a5510aed51da9f32505
XieQian <489340274@qq.com>
2017-03-07 18:33:19 +0800

    fullscreen

--- app/src/main/java/com/dc/transmission/MainActivity.java
+++ app/src/main/java/com/dc/transmission/MainActivity.java
@@ -1,13 +1,23 @@
 package com.dc.transmission;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume(){
+        if(getRequestedOrientation()!= ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE)
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+        super.onResume();
     }
 }
