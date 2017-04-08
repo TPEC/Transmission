d4e1b68d8ab714e4291662b9d1dac9670cb41039
XieQian <489340274@qq.com>
2017-04-08 11:46:13 +0800

    task

--- app/src/main/java/com/dc/transmission/glObjects/HorizontalFloors.java
+++ app/src/main/java/com/dc/transmission/glObjects/HorizontalFloors.java
@@ -12,11 +12,7 @@ public class HorizontalFloors implements TGLObjects {
     }
 
     public void calcPhysics(float[] pos, float[] vel){
-        for(int i=0;i<floors.length;i++){
-            if(pos[0]>=floors[i].left && pos[0]<floors[i].right && pos[2]>=floors[i].top && pos[2]<floors[i].bottom){
 
-            }
-        }
     }
 
     @Override
